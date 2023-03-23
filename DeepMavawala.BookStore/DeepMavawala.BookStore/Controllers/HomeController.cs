936368e297a70d4faf61fc0bc9e7e8b0f514166e
deep.mavavala <deep.mavavala@01-CPU-A604>
2023-03-23 11:07:28 +0530

    Added Mvc in Application

---
+++ DeepMavawala.BookStore/DeepMavawala.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeepMavawala.BookStore.Controllers
+{
+	public class HomeController : Controller
+	{
+		public string Index()
+		{
+			return "Deep Mavawala";
+		}
+	}
+}
